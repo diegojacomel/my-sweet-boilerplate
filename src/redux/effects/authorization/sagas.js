fe2ad33f9db964a7626feb2fc3fc405b3cf96e6b
Debora de Lima <delima04@prestadores.amil.com.br>
2018-01-05 09:37:11 -0200

    adicionado mock menu

---
+++ src/redux/effects/authorization/sagas.js
@@ -0,0 +1,17 @@
+import { call, put, takeEvery } from 'redux-saga/effects';
+import AuthorizationService from '../../../services/authorization';
+
+function* fetchMenu(action) {
+    try {
+        const menu = yield call(AuthorizationService.getMenu);
+        
+        yield put({ type: "FETCH_MENU_SUCCESS", menu: menu.data });
+    } catch (e) {
+        yield put({ type: "FETCH_MENU_FAILURE", message: e.message });
+    }
+}
+
+// here we can pass an array of sagas to export to the rootSagas
+export const userSagas = [
+    takeEvery("FETCH_MENU_USER", fetchMenu)
+];
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import usersReducer from './users'
+import authorizationReducer from './authorization'
 
 // all the reducers in one place
 const rootReducers = combineReducers({
     users: usersReducer,
+    authorization: authorizationReducer,
     form: formReducer
 })
 
